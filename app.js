30f0ccf58577c3c63bb677b69f7bcc6210157288
codemiz <ik571572@gmail.com>
2024-05-27 15:37:58 +0500

    update

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require("./models/user")
+const cookieParser = require('cookie-parser')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}))
 
 //main routes
-app.get('/', (req, res) => {
-  res.render("home");
+app.get('/',isLoggedIn, (req, res) => {
+  if(isLoggedIn){
+    res.render("home")
+  }else{
+    res.render("login");
+
+  }
+  
 });
 app.get('/register', (req, res) => {
   res.render('register');
@@ -21,15 +30,32 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/profile', (req, res) => {
+  console.log(req.cookies.token)
   res.render('profile');
 });
 
 //Api routes
 app.post("/user/signup",(req,res)=>{
+  const {name,username,email,password,age,gender} = req.body;
+  const newUser = userModel.create({
+    name,
+    username,
+    email,
+    password,
+    age,
+    gender
+  })
   console.log(req.body);
-  res.send("done")
+  res.redirect("/")
 })
 
+
+//loggedin check
+function isLoggedIn(){
+  
+    return true
+  
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/justpostDb");
+
+const userSchema = mongoose.Schema({
+    name: String,
+    username:{
+        type : String,
+        unique:true
+    },
+    email:String,
+    password:String,
+    age:Number,
+    gender:String,
+    posts:[
+
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"post"
+        }
+    ]
+})
+module.exports = mongoose.model("user",userSchema);
