7564b28f7ebbe694c3b999ea04f245b798dafcc6
codemiz <ik571572@gmail.com>
2025-07-09 16:51:15 +0500

    changes

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI_SERVER);
 
 const userSchema = mongoose.Schema({
     name: String,
